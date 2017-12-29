d0765916efeda74b261025ad12938c2c19181024
Mr.  Huang <2984628240@qq.com>
2017-12-29 09:34:53 +0800

    添加注释

--- drivershow/蜂鸣器驱动/test.c
+++ drivershow/蜂鸣器驱动/test.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<fcntl.h>
 #include<string.h>
-#include<sys/ioctl.h>
+#include<sys/ioctl.h>/*ioctl()是I/O操作的杂货箱，很多事情都要依靠它来完成*/
 int main(void)
 {
 	int fpd;
@@ -9,6 +9,8 @@ int main(void)
 	char buffer[200]="hello,everyone\n";
 	fpd=open("/dev/hello",O_CREAT|O_RDWR,0X666);
 #if 0
+	/向设备发送数据，成功时该函数返回写入的字节数。如果此函数未被实现，当用户进行
+	write()系统调用时，将得到-EINVAL 返回值。*/
 	retur=write(fpd,buffer,strlen(buffer)+1);	
 	printf("user write retur= %d\n",retur);
 	memset(buffer,'\0',sizeof(buffer));
